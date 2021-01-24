8806d1d3b81b36df58f6f31d0ccc3be0f0306067
Encouragements <encouragementsforall@gmail.com>
2021-01-24 16:03:01 -0600

    Latest

---
+++ shared/src/commonMain/kotlin/org/encouragement/app/shared/EncouragementRepository.kt
@@ -0,0 +1,17 @@
+package org.encouragement.app.shared
+
+import io.ktor.client.*
+import io.ktor.client.request.*
+
+object EncouragementRepository {
+    private const val BASE_URL =
+        "https://raw.githubusercontent.com/encouragements/encouragement-app/main/encouragements"
+    private const val LATEST_POINTER_URL = "$BASE_URL/latest"
+
+    suspend fun getLatestEnc(): String {
+        val client = HttpClient()
+        val latestPointer = client.get<String>(LATEST_POINTER_URL)
+        val latestUrl = "$BASE_URL/$latestPointer"
+        return client.get(latestUrl)
+    }
+}
